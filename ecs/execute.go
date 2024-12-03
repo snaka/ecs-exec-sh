51bbe394a20fed1ae54b4a35ff79b45072501533
Shinji Nakamatsu <19329+snaka@users.noreply.github.com>
2024-12-03 22:26:14 +0900

    Call session-manager-plugin directly instead of ecsta

--- ecs/execute.go
+++ ecs/execute.go
@@ -2,11 +2,16 @@ package ecs
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
+	"os"
+	"os/exec"
+	"strings"
+	"time"
 
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/service/ecs"
-	"github.com/fujiwara/ecsta"
+	"github.com/aws/aws-sdk-go-v2/service/ecs/types"
 )
 
 func ExecuteCommand(clusterName, serviceName, containerName, command string) error {
@@ -31,24 +36,80 @@ func ExecuteCommand(clusterName, serviceName, containerName, command string) err
 
     fmt.Printf("Task: %s\n", taskID)
 
-    ecstaApp, err := newEcsta(ctx, "ap-northeast-1", clusterName)
-    if err != nil {
-        return fmt.Errorf("failed to create Ecsta: %w", err)
-    }
-
-    // Execute command
-    return ecstaApp.RunExec(ctx,&ecsta.ExecOption{
-        ID: taskID,
-        Service: &serviceName,
-        Container: containerName,
-        Command: command,
-    })
+    return runExec(ctx, svc, clusterName, taskID, containerName, command)
 }
 
-func newEcsta(ctx context.Context, region, cluster string) (*ecsta.Ecsta, error) {
-    app, err := ecsta.New(ctx, region, cluster)
+func runExec(ctx context.Context, svc *ecs.Client, clusterName, taskID, containerName, command string) error {
+    out, err := svc.ExecuteCommand(ctx, &ecs.ExecuteCommandInput{
+        Cluster:     aws.String(clusterName),
+        Interactive: true,
+        Task:       aws.String(taskID),
+        Command:     aws.String(command),
+        Container:   aws.String(containerName),
+    })
     if err != nil {
-        return nil, fmt.Errorf("failed to create Ecsta, %w", err)
+        return fmt.Errorf("failed to execute command: %w", err)
     }
-    return app, nil
+
+    tasks, err := svc.DescribeTasks(ctx, &ecs.DescribeTasksInput{
+        Cluster: aws.String(clusterName),
+        Tasks:   []string{taskID},
+    })
+    if err != nil {
+        return fmt.Errorf("failed to describe task: %w", err)
+    }
+    task := tasks.Tasks[0]
+
+    target, err := ssmRequestTarget(task, containerName)
+    if err != nil {
+        return fmt.Errorf("failed to get SSM target: %w", err)
+    }
+    return runSessionManagerPlugin(ctx, out.Session, target)
+}
+
+func ssmRequestTarget(task types.Task, targetContainer string) (string, error) {
+    value := strings.Split(*task.TaskArn, "/")
+    clusterName := value[1]
+    taskID := value[2]
+    var runtimeID string
+    for _, c := range task.Containers {
+        if *c.Name == targetContainer {
+            runtimeID = *c.RuntimeId
+        }
+    }
+    return fmt.Sprintf("ecs:%s_%s_%s", clusterName, taskID, runtimeID), nil
+}
+
+func runSessionManagerPlugin(ctx context.Context, session *types.Session, target string) error {
+    sess, err := json.Marshal(session)
+    if err != nil {
+        return fmt.Errorf("failed to marshal session: %w", err)
+    }
+    ssmReq, err := json.Marshal(map[string]string{
+        "Target": target,
+    })
+    if err != nil {
+        return fmt.Errorf("failed to marshal ssm request: %w", err)
+    }
+
+    ctx, cancel := context.WithCancel(ctx)
+    defer cancel()
+    cmd := exec.CommandContext(
+        ctx,
+        "session-manager-plugin",
+        string(sess),
+        "ap-northeast-1",
+        "StartSession",
+        "",
+        string(ssmReq),
+    )
+    // send SIGINT to the process when the context is canceled
+    cmd.Cancel = func() error {
+        return cmd.Process.Signal(os.Interrupt)
+    }
+    cmd.WaitDelay = 3 * time.Second
+    cmd.Stdin = os.Stdin
+    cmd.Stdout = os.Stdout
+    cmd.Stderr = os.Stderr
+    return cmd.Run()
 }
--- go.mod
+++ go.mod
@@ -6,45 +6,23 @@ require (
 	github.com/aws/aws-sdk-go-v2 v1.32.5
 	github.com/aws/aws-sdk-go-v2/config v1.28.5
 	github.com/aws/aws-sdk-go-v2/service/ecs v1.52.0
-	github.com/fujiwara/ecsta v0.5.1
 	github.com/spf13/cobra v1.8.1
 )
 
 require (
-	github.com/Songmu/flextime v0.1.0 // indirect
-	github.com/Songmu/prompter v0.5.1 // indirect
-	github.com/alecthomas/kong v0.9.0 // indirect
-	github.com/aws/aws-sdk-go-v2/aws/protocol/eventstream v1.6.4 // indirect
 	github.com/aws/aws-sdk-go-v2/credentials v1.17.46 // indirect
 	github.com/aws/aws-sdk-go-v2/feature/ec2/imds v1.16.20 // indirect
 	github.com/aws/aws-sdk-go-v2/internal/configsources v1.3.24 // indirect
 	github.com/aws/aws-sdk-go-v2/internal/endpoints/v2 v2.6.24 // indirect
 	github.com/aws/aws-sdk-go-v2/internal/ini v1.8.1 // indirect
-	github.com/aws/aws-sdk-go-v2/service/cloudwatchlogs v1.37.4 // indirect
 	github.com/aws/aws-sdk-go-v2/service/internal/accept-encoding v1.12.1 // indirect
 	github.com/aws/aws-sdk-go-v2/service/internal/presigned-url v1.12.5 // indirect
-	github.com/aws/aws-sdk-go-v2/service/sns v1.26.7 // indirect
-	github.com/aws/aws-sdk-go-v2/service/ssm v1.52.5 // indirect
 	github.com/aws/aws-sdk-go-v2/service/sso v1.24.6 // indirect
 	github.com/aws/aws-sdk-go-v2/service/ssooidc v1.28.5 // indirect
 	github.com/aws/aws-sdk-go-v2/service/sts v1.33.1 // indirect
 	github.com/aws/smithy-go v1.22.1 // indirect
-	github.com/creack/pty v1.1.23 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
-	github.com/fujiwara/tracer v1.1.0 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
-	github.com/itchyny/gojq v0.12.16 // indirect
-	github.com/itchyny/timefmt-go v0.1.6 // indirect
 	github.com/jmespath/go-jmespath v0.4.0 // indirect
-	github.com/mattn/go-isatty v0.0.20 // indirect
-	github.com/mattn/go-runewidth v0.0.15 // indirect
-	github.com/olekukonko/tablewriter v0.0.5 // indirect
-	github.com/rivo/uniseg v0.4.7 // indirect
-	github.com/samber/lo v1.47.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
-	github.com/tkuchiki/go-timezone v0.2.2 // indirect
-	github.com/tkuchiki/parsetime v0.3.0 // indirect
-	golang.org/x/sys v0.22.0 // indirect
-	golang.org/x/term v0.22.0 // indirect
-	golang.org/x/text v0.16.0 // indirect
 )
